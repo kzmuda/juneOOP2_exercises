014b26bcd89a2c322ed0d11cdeafc33317b8a2bd
kzmuda <kh@wp.pl>
2023-01-22 13:51:01 +0100

    Add generic class DataStore.

---
+++ juneOOP2_exercises/DataStore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juneOOP2_exercises
+{
+    public class DataStore<T>
+    {
+        public T Data { get; set; }
+
+        public void Set(T data)
+        {
+        }
+
+        public T Get()
+        {
+            return Data;
+        }
+
+        /*
+         *
+         */
+    }
+}
--- juneOOP2_exercises/Program.cs
+++ juneOOP2_exercises/Program.cs
@@ -2,8 +2,55 @@
 {
     internal class Program
     {
+        static void PrintArray(int[] arr)
+        {
+            ///
+            foreach (var element in arr)
+            {
+                Console.Out.WriteLine(element);
+            }
+        }
+
+        static void PrintArray(char[] arr)
+        {
+            ///
+            foreach (var element in arr)
+            {
+                Console.Out.WriteLine(element);
+            }
+        }
+
+        static void PrintArray<T>(T b)
+        {
+            T a;
+        }
+
         static void Main(string[] args)
         {
+            // DataStore<int> ds = new DataStore<int>();
+            // ds.Data = 10;
+            // //ds.Data = "aaaa";
+            //
+            // DataStore<string> ds1 = new DataStore<string>();
+            // //ds1.Data = 10;
+            // ds1.Data = "aaa";
+
+
+
+            // int[] intArray = { 1,2,3,4 };
+            // PrintArray(intArray);
+            //
+            // char[] charArray = { 'a', 'b' };
+            // PrintArray(charArray);
+            //
+            // PrintArray<int>(5);
+            // PrintArray<string>("aaaa");
+
+
+            // List<int> a;
+            // List<string> b;
+            // List<Complex> c;
+
             /*
             // Task 1
             Complex mycomplex = new Complex(2, 0);
@@ -45,6 +92,7 @@
             */
 
             // Task 3
+            /*
             List<Complex> complex = new List<Complex>()
             {
                 new Complex(5, 3),
@@ -60,6 +108,7 @@
             {
                 Console.Out.WriteLine(c);
             }
+            */
         }
     }
 }
