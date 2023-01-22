5f5b91a5dcaff57103e468a2a13e577e57d2b8de
Milena Kolakowska <nekoloid@gmail.com>
2023-01-22 10:10:19 +0100

    first

---
+++ juneOOP2_exercises/Complex.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juneOOP2_exercises
+{
+    public class Complex
+    {
+        private int real;
+        private int imaginary;
+
+        public Complex(int realNumber, int imaginaryNumber) {
+            this.real = realNumber;
+            this.imaginary = imaginaryNumber;
+        }
+
+        public Complex(int realNumber)
+        {
+            this.real = realNumber;
+            this.imaginary = 0;
+        }
+
+        public override string ToString()
+        {
+            if (imaginary == 0)
+            {
+                return real.ToString();
+            }
+            else
+            {
+                return real + "+" + imaginary + "i";
+            }
+        }
+
+        public static Complex AddTwo(Complex a, Complex b)
+        {
+            int sumReal = a.real + b.real;
+            int sumImaginary = a.imaginary + b.imaginary;
+            Complex newComplex = new Complex(sumReal, sumImaginary);
+            return newComplex;
+        }
+
+        public void IncreaseReal(int increase)
+        {
+            this.real += increase;
+            //return this;
+        }
+
+        public void AddComplex(Complex a)
+        {
+            this.real += a.real;
+            this.imaginary += a.imaginary;
+        }
+    }
+}
--- juneOOP2_exercises/Program.cs
+++ juneOOP2_exercises/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Complex mycomplex = new Complex(2, 0);
+            //Console.WriteLine(mycomplex);
+            Complex mycomplex2 = new Complex(2, 4);
+            //Console.WriteLine(Complex.AddTwo(mycomplex, mycomplex2));
+            mycomplex.IncreaseReal(2);
+            Console.WriteLine(mycomplex);
+            mycomplex.AddComplex(mycomplex2);
+            Console.WriteLine(mycomplex); // 6 + 4i
         }
     }
 }
